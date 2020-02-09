1695b7ec8fe7c21c69a1cc7b175864db0b6b123d
lzlocki <lzlocki@poczta.fm>
2020-02-09 09:32:43 +0100

    add identity iside areas

---
+++ InvestWallet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InvestWallet.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InvestWallet.Areas.Identity.IdentityHostingStartup))]
+namespace InvestWallet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
