17c83a1fc2c73e9cd8d83350490ff9860c57ae06
lzlocki <lzlocki@poczta.fm>
2020-02-09 07:53:28 +0100

    Create Wallet and Transaction model

---
+++ InvestWallet/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestWallet.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public String StockName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime BuyDate { get; set; }
+        public DateTime SaleDate { get; set; }
+        public double Sellprice { get; set; }
+        public int SellQuantity { get; set; }
+
+    }
+}
---
+++ InvestWallet/Models/Wallet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestWallet.Models
+{
+    public class Wallet
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Wallet name")]
+        [Required]
+        public string description { get; set; }
+
+        //reference to transactions
+        [Required]
+        [Display(Name="Transaction")]
+        public int TransactionId { get; set; }
+
+        [ForeignKey("TransactionId")]
+        public virtual Transaction Transaction { get; set; }
+
+    }
+}
